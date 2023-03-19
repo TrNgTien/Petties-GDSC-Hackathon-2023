97e40df201581b73c61b7301f019832e9dc3228a
Nguyenle23___ <beenbin2308@gmail.com>
2023-03-19 17:39:16 +0700

    remove: redundant

--- server/src/routes/ChatRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const chatHandler = require("../controllers/ChatHandler");
-
-router.post("/createRoom", chatHandler.createRoom);
-
-module.exports = router;
