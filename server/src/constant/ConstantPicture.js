339559a77f84e773788fa6a637085078878ea358
Nguyenle23___ <beenbin2308@gmail.com>
2023-03-19 10:48:10 +0700

    remove: redundant file

--- server/src/constant/ConstantPicture.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    BLANK_COVER: "https://facebook-clone-hcmiu.s3.ap-southeast-1.amazonaws.com/avatar/5pqSLR.jpg",
-    BLANK_COVER_KEY: "avatar/5pqSLR.jpg",
-    BLANK_AVATAR: "https://facebook-clone-hcmiu.s3.ap-southeast-1.amazonaws.com/avatar/istockphoto-1288129985-612x612_nhuday.jpg",
-    BLANK_AVATAR_KEY: "avatar/istockphoto-1288129985-612x612_nhuday.jpg"
-}
